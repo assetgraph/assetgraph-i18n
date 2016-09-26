c960bf3aee0d486973cbf15a6e8d8fbcf75ddefa
Andreas Lind <andreas@one.com>
2016-09-26 13:40:28 +0200

    fix it

--- i18n.js
+++ i18n.js
@@ -241,7 +241,7 @@ function createTr(localeData) {
     TR.HTML = function (htmlString) {
         var div = document.createElement('div');
         div.innerHTML = htmlString;
-        require('./lib/eachI18nTagInHtmlDocument')(div, require('./lib/createI18nTagReplacer')({
+        eachI18nTagInHtmlDocument(div, createI18nTagReplacer({
             allKeysForLocale: localeData,
             keepI18nAttributes: true,
             keepSpans: true
