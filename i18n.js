0e43d9b24d447a82cff6042d114572c26ac0b5db
Andreas Lind <andreas@one.com>
2016-09-26 13:49:01 +0200

    Revert "Maintain tokenizePattern, eachI18nTagInHtmlDocument, and createI18nTagReplacer in the system.js plugin."
    This reverts commit 42e7f3c905c71d0d955c139198b674c8c3740169.

--- i18n.js
+++ i18n.js
@@ -18,173 +18,6 @@ function expandLocaleIdToPrioritizedList(localeId) {
     return localeIds;
 }
 
-function tokenizePattern(pattern) {
-    if (typeof pattern !== 'string') {
-        var valueString = pattern;
-        try {
-            valueString = JSON.stringify(pattern);
-        } catch (e) {}
-        throw new Error('i18nTools.tokenizePattern: Value must be a string: ' + valueString);
-    }
-    var tokens = [],
-        fragments = pattern.split(/(\{\d+\})/);
-    for (var i = 0 ; i < fragments.length ; i += 1) {
-        var fragment = fragments[i];
-        if (fragment.length > 0) {
-            var matchPlaceHolder = fragment.match(/^\{(\d+)\}$/);
-            if (matchPlaceHolder) {
-                tokens.push({
-                    type: 'placeHolder',
-                    value: parseInt(matchPlaceHolder[1], 10)
-                });
-            } else {
-                tokens.push({
-                    type: 'text',
-                    value: fragment
-                });
-            }
-        }
-    }
-    return tokens;
-}
-
-function eachI18nTagInHtmlDocument(document, lambda, nestedTemplateLambda) {
-    var ELEMENT_NODE = 1,
-        TEXT_NODE = 3,
-        queue = [document],
-        i;
-    while (queue.length) {
-        var node = queue.shift(),
-            parentNode = node.parentNode,
-            nodeStillInDocument = true;
-        if (parentNode && node.nodeType === ELEMENT_NODE) {
-            if (node.hasAttribute && node.hasAttribute('data-i18n')) { // In IE7 the HTML node doesn't have a hasAttribute method?
-                var i18nStr = node.getAttribute('data-i18n'),
-                    i18nObj;
-
-                if (i18nStr.indexOf(':') !== -1) {
-                    try {
-                        i18nObj = eval('({' + i18nStr + '})'); // eslint-disable-line no-eval
-                    } catch (e) {
-                        throw new Error('i18nTools.eachI18nTagInHtmlDocument: Error evaluating data-i18n attribute: ' + i18nStr + '\n' + e.stack);
-                    }
-                } else {
-                    i18nObj = {text: i18nStr};
-                }
-
-                if (i18nObj.attr) {
-                    var attributeNames = Object.keys(i18nObj.attr);
-                    for (i = 0 ; i < attributeNames.length ; i += 1) {
-                        var attributeName = attributeNames[i],
-                            key = i18nObj.attr[attributeName] || null;
-                        if (lambda({type: 'i18nTagAttribute', attributeName: attributeName, node: node, key: key, defaultValue: node.getAttribute(attributeName)}) === false) {
-                            return;
-                        }
-                    }
-                }
-
-                if (typeof i18nObj.text !== 'undefined') {
-                    var defaultValue = '',
-                        placeHolders = [],
-                        nextPlaceHolderNumber = 0;
-
-                    for (i = 0 ; i < node.childNodes.length ; i += 1) {
-                        var childNode = node.childNodes[i];
-                        if (childNode.nodeType === TEXT_NODE) {
-                            defaultValue += childNode.nodeValue;
-                        } else {
-                            defaultValue += '{' + nextPlaceHolderNumber + '}';
-                            nextPlaceHolderNumber += 1;
-                            placeHolders.push(childNode);
-                        }
-                    }
-                    defaultValue = defaultValue.replace(/^[ \n\t]+|[ \n\t]+$/g, ''); // Trim leading and trailing whitespace, except non-breaking space chars
-                    defaultValue = defaultValue.replace(/[ \n\t]+/g, ' '); // Compress and normalize sequences of 1+ spaces to one ' '
-                    if (lambda({type: 'i18nTagText', node: node, key: i18nObj.text || null, defaultValue: defaultValue, placeHolders: placeHolders}) === false) {
-                        return;
-                    }
-                } else {
-                    // A tag with a data-i18n tag, but no language key for the text contents.
-                    // Give the lambda a chance to clean up the tag anyway:
-                    lambda({node: node});
-                }
-                if (!node.parentNode) {
-                    nodeStillInDocument = false;
-                    queue.unshift(parentNode);
-                }
-            }
-            // Give the caller a chance to do something about nested <script type="text/html">...</script> templates (used by TRHTML in the browser):
-            if (nestedTemplateLambda && node.nodeName.toLowerCase() === 'script' && node.getAttribute('type') === 'text/html') {
-                nestedTemplateLambda(node);
-            }
-        }
-        if (nodeStillInDocument && node.childNodes) {
-            for (i = node.childNodes.length - 1 ; i >= 0 ; i -= 1) {
-                queue.unshift(node.childNodes[i]);
-            }
-        }
-    }
-}
-
-function createI18nTagReplacer(options) {
-    var TEXT_NODE = 3,
-        allKeysForLocale = options.allKeysForLocale,
-        keepI18nAttributes = options.keepI18nAttributes,
-        keepSpans = options.keepSpans;
-
-    return function i18nTagReplacer(options) {
-        var key = options.key,
-            node = options.node,
-            value = allKeysForLocale[key],
-            removeNode = !keepSpans && options.type !== 'i18nTagAttribute' && node.nodeName.toLowerCase() === 'span' && node.attributes.length === 1;
-
-        if (key !== null) { // An empty string or null means explicitly "do not translate"
-            if (/^i18nTag/.test(options.type) && value === null || typeof value === 'undefined') {
-                value = options.defaultValue || '[!' + key + '!]';
-            }
-            if (options.type === 'i18nTagAttribute') {
-                node.setAttribute(options.attributeName, value);
-            } else if (options.type === 'i18nTagText') {
-                while (node.childNodes.length) {
-                    node.removeChild(node.firstChild);
-                }
-                tokenizePattern(value).forEach(function (token) {
-                    var nodeToInsert;
-                    if (token.type === 'text') {
-                        nodeToInsert = node.ownerDocument.createTextNode(token.value);
-                    } else {
-                        var placeHolder = options.placeHolders[token.value];
-                        if (placeHolder) {
-                            nodeToInsert = placeHolder;
-                            if (nodeToInsert.parentNode) {
-                                nodeToInsert = nodeToInsert.cloneNode(true);
-                            }
-                        } else {
-                            nodeToInsert = node.ownerDocument.createTextNode('[!{' + token.value + '}!]');
-                        }
-                    }
-                    if (removeNode) {
-                        if (nodeToInsert.nodeType === TEXT_NODE && node.previousSibling && node.previousSibling.nodeType === TEXT_NODE) {
-                            // Splice with previous text node
-                            node.previousSibling.nodeValue += nodeToInsert.nodeValue;
-                        } else {
-                            node.parentNode.insertBefore(nodeToInsert, node);
-                        }
-                    } else {
-                        node.appendChild(nodeToInsert);
-                    }
-                });
-            }
-        }
-        if (removeNode) {
-            node.parentNode.removeChild(node);
-        } else if (!keepI18nAttributes && options.type !== 'i18nTagAttribute') {
-            node.removeAttribute('data-i18n');
-        }
-    };
-}
-
-
 function createTr(localeData) {
     function tokenizePattern(pattern) {
         if (typeof pattern !== 'string') {
@@ -214,6 +47,142 @@ function createTr(localeData) {
             }
         }
         return tokens;
+    }
+
+    function eachI18nTagInHtmlDocument(document, lambda, nestedTemplateLambda) {
+        var ELEMENT_NODE = 1,
+            TEXT_NODE = 3,
+            queue = [document],
+            i;
+        while (queue.length) {
+            var node = queue.shift(),
+                parentNode = node.parentNode,
+                nodeStillInDocument = true;
+            if (parentNode && node.nodeType === ELEMENT_NODE) {
+                if (node.hasAttribute && node.hasAttribute('data-i18n')) { // In IE7 the HTML node doesn't have a hasAttribute method?
+                    var i18nStr = node.getAttribute('data-i18n'),
+                        i18nObj;
+
+                    if (i18nStr.indexOf(':') !== -1) {
+                        try {
+                            i18nObj = eval('({' + i18nStr + '})'); // eslint-disable-line no-eval
+                        } catch (e) {
+                            throw new Error('eachI18nTagInHtmlDocument: Error evaluating data-i18n attribute: ' + i18nStr + '\n' + e.stack);
+                        }
+                    } else {
+                        i18nObj = {text: i18nStr};
+                    }
+
+                    if (i18nObj.attr) {
+                        var attributeNames = Object.keys(i18nObj.attr);
+                        for (i = 0 ; i < attributeNames.length ; i += 1) {
+                            var attributeName = attributeNames[i],
+                                key = i18nObj.attr[attributeName] || null;
+                            if (lambda({type: 'i18nTagAttribute', attributeName: attributeName, node: node, key: key, defaultValue: node.getAttribute(attributeName)}) === false) {
+                                return;
+                            }
+                        }
+                    }
+
+                    if (typeof i18nObj.text !== 'undefined') {
+                        var defaultValue = '',
+                            placeHolders = [],
+                            nextPlaceHolderNumber = 0;
+
+                        for (i = 0 ; i < node.childNodes.length ; i += 1) {
+                            var childNode = node.childNodes[i];
+                            if (childNode.nodeType === TEXT_NODE) {
+                                defaultValue += childNode.nodeValue;
+                            } else {
+                                defaultValue += '{' + nextPlaceHolderNumber + '}';
+                                nextPlaceHolderNumber += 1;
+                                placeHolders.push(childNode);
+                            }
+                        }
+                        defaultValue = defaultValue.replace(/^[ \n\t]+|[ \n\t]+$/g, ''); // Trim leading and trailing whitespace, except non-breaking space chars
+                        defaultValue = defaultValue.replace(/[ \n\t]+/g, ' '); // Compress and normalize sequences of 1+ spaces to one ' '
+                        if (lambda({type: 'i18nTagText', node: node, key: i18nObj.text || null, defaultValue: defaultValue, placeHolders: placeHolders}) === false) {
+                            return;
+                        }
+                    } else {
+                        // A tag with a data-i18n tag, but no language key for the text contents.
+                        // Give the lambda a chance to clean up the tag anyway:
+                        lambda({node: node});
+                    }
+                    if (!node.parentNode) {
+                        nodeStillInDocument = false;
+                        queue.unshift(parentNode);
+                    }
+                }
+                // Give the caller a chance to do something about nested <script type="text/html">...</script> templates (used by TRHTML in the browser):
+                if (nestedTemplateLambda && node.nodeName.toLowerCase() === 'script' && node.getAttribute('type') === 'text/html') {
+                    nestedTemplateLambda(node);
+                }
+            }
+            if (nodeStillInDocument && node.childNodes) {
+                for (i = node.childNodes.length - 1 ; i >= 0 ; i -= 1) {
+                    queue.unshift(node.childNodes[i]);
+                }
+            }
+        }
+    }
+
+    function createI18nTagReplacer(options) {
+        var TEXT_NODE = 3,
+            allKeysForLocale = options.allKeysForLocale,
+            keepI18nAttributes = options.keepI18nAttributes,
+            keepSpans = options.keepSpans;
+
+        return function i18nTagReplacer(options) {
+            var key = options.key,
+                node = options.node,
+                value = allKeysForLocale[key],
+                removeNode = !keepSpans && options.type !== 'i18nTagAttribute' && node.nodeName.toLowerCase() === 'span' && node.attributes.length === 1;
+
+            if (key !== null) { // An empty string or null means explicitly "do not translate"
+                if (/^i18nTag/.test(options.type) && value === null || typeof value === 'undefined') {
+                    value = options.defaultValue || '[!' + key + '!]';
+                }
+                if (options.type === 'i18nTagAttribute') {
+                    node.setAttribute(options.attributeName, value);
+                } else if (options.type === 'i18nTagText') {
+                    while (node.childNodes.length) {
+                        node.removeChild(node.firstChild);
+                    }
+                    tokenizePattern(value).forEach(function (token) {
+                        var nodeToInsert;
+                        if (token.type === 'text') {
+                            nodeToInsert = node.ownerDocument.createTextNode(token.value);
+                        } else {
+                            var placeHolder = options.placeHolders[token.value];
+                            if (placeHolder) {
+                                nodeToInsert = placeHolder;
+                                if (nodeToInsert.parentNode) {
+                                    nodeToInsert = nodeToInsert.cloneNode(true);
+                                }
+                            } else {
+                                nodeToInsert = node.ownerDocument.createTextNode('[!{' + token.value + '}!]');
+                            }
+                        }
+                        if (removeNode) {
+                            if (nodeToInsert.nodeType === TEXT_NODE && node.previousSibling && node.previousSibling.nodeType === TEXT_NODE) {
+                                // Splice with previous text node
+                                node.previousSibling.nodeValue += nodeToInsert.nodeValue;
+                            } else {
+                                node.parentNode.insertBefore(nodeToInsert, node);
+                            }
+                        } else {
+                            node.appendChild(nodeToInsert);
+                        }
+                    });
+                }
+            }
+            if (removeNode) {
+                node.parentNode.removeChild(node);
+            } else if (!keepI18nAttributes && options.type !== 'i18nTagAttribute') {
+                node.removeAttribute('data-i18n');
+            }
+        };
     }
 
     var TR = function (key, defaultValue) {
@@ -272,12 +241,6 @@ function gatherKeysForLocale(source, locale) {
 }
 
 module.exports = {
-    // These are maintained here because we haven't been able to work out how
-    // to use relative requires in a system.js plugin:
-    tokenizePattern: tokenizePattern,
-    eachI18nTagInHtmlDocument: eachI18nTagInHtmlDocument,
-    createI18nTagReplacer: createI18nTagReplacer,
-
     // fetch: sideeffect: load relevant parts of i18n file filtered by locale
     fetch: function (load, fetch) {
         load.metadata.newAddress = load.address.replace(/\.([^.]+)\.i18n/, function (str, matchedLocale) {
